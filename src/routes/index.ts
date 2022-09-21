4010bf16b72c2b631af5e692ff4f43ecc75d406a
Oscar_MejiaSoc4 <mejiaoscar43@gmail.com>
2022-09-20 19:13:31 -0600

    Added Test Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,38 @@ router.get('/', (_req, res) => {
   res.json({msg:'Hello World!'});
  });
 
+router.get('/hello', (req, res)=>{
+  const {name = 'No Name Found'} = req.query;
+  res.json({
+    name
+  });
+});
+
+router.get('/view/:id', (req, res)=>{
+  const { id = -1 } = req.params as unknown as {id?: number};
+  res.json({id});
+});
+
+router.post('/new', (req, res)=>{
+  //Conversión de Datos
+  const {name = 'No Name Found', email = "", age = 0} = req.body as {name?: String, email?: String, age?: number};
+  
+  res.json({name, email, age});
+});
+
+// Variables en URL
+router.put('/update/:id', (req, res)=>{
+  const { id = -1 } = req.params as unknown as {id?:number};
+  const {name = 'No Name Found', email = "", age = 0} = req.body as {name?: String, email?: String, age?: number};
+
+  res.json({name, email, age});
+});
+
+router.put('/delete/:id', (req, res)=>{
+
+  const { id = -1 } = req.params as unknown as {id?:number};
+
+  res.json({id});
+});
+
 export default router;
