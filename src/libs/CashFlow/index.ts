650809e20b452e9ec75f600250ca0eea34a02136
Oscar_MejiaSoc4 <mejiaoscar43@gmail.com>
2022-09-22 18:57:05 -0600

    Basic CRUD

---
+++ src/libs/CashFlow/index.ts
@@ -0,0 +1,65 @@
+/* Estructura de datos inicial */
+export interface ICashFlow {
+    type: 'INCOME' | 'EXPENSE';
+    date: Date;
+    amount: number;
+    description: String;
+}
+
+/* Manejo en memoria de un objeto */
+export class CashFlow {
+    private cashFlowItems: ICashFlow[] = [];
+
+    /* Consultas */
+    /**
+     * getAllCashFlow
+    */
+    public getAllCashFlow(): ICashFlow[] {
+        return this.cashFlowItems;
+    }
+
+    public getCashFlowByIndex(index:number): ICashFlow {
+        if (index >= 0 && index < this.cashFlowItems.length) {
+            
+            return this.cashFlowItems[index];
+        }
+        throw Error("Index out of range");
+        
+    }
+
+    public addCashFlow(cashFlow: ICashFlow ): number {
+        const cashFlowExists = this.cashFlowItems.findIndex(
+            (obj) => {
+                return obj.amount === cashFlow.amount && obj.description === cashFlow.description;
+            }
+        )
+        
+        if (cashFlowExists < 0) {
+            this.cashFlowItems.push(cashFlow);    
+            return this.cashFlowItems.length - 1; 
+        }
+
+        throw Error('Error: CashFlow already Exists on Collection.');
+
+    }
+    
+    public updateCashFlow(index:number, cashFlow:ICashFlow): boolean {
+        if (index >= 0 && index < this.cashFlowItems.length) {
+            this.cashFlowItems[index] = cashFlow;
+            return true;
+        }
+        return false;
+    }
+
+    public deleteCashFlow(index:number):boolean {
+        //Agregar _ al inicio de la variable para indicar que no se va usar.
+        if (index >= 0 && index < this.cashFlowItems.length) {
+            //Usar filter para evitar la mutabilidad de los datos.
+            this.cashFlowItems = this.cashFlowItems.filter(
+                (_obj: ICashFlow, i:number) => i !== index
+            );
+            return true;
+        }
+        return false;
+    }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,42 +1,8 @@
-import express from 'express';
-const router  = express.Router();
+import {Router} from 'express';
+import CashFlowRouter from './CashFlows';
 
-router.get('/', (_req, res) => {
-  res.json({msg:'Hello World!'});
- });
+const router  = Router();
 
-router.get('/hello', (req, res)=>{
-  const {name = 'No Name Found'} = req.query;
-  res.json({
-    name
-  });
-});
-
-router.get('/view/:id', (req, res)=>{
-  const { id = -1 } = req.params as unknown as {id?: number};
-  res.json({id});
-});
-
-router.post('/new', (req, res)=>{
-  //Conversión de Datos
-  const {name = 'No Name Found', email = "", age = 0} = req.body as {name?: String, email?: String, age?: number};
-  
-  res.json({name, email, age});
-});
-
-// Variables en URL
-router.put('/update/:id', (req, res)=>{
-  const { id = -1 } = req.params as unknown as {id?:number};
-  const {name = 'No Name Found', email = "", age = 0} = req.body as {name?: String, email?: String, age?: number};
-
-  res.json({name, email, age});
-});
-
-router.put('/delete/:id', (req, res)=>{
-
-  const { id = -1 } = req.params as unknown as {id?:number};
-
-  res.json({id});
-});
+router.use('/cashflow', CashFlowRouter);
 
 export default router;
